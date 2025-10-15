11c12f3b33d654796ef8e26907bc687276d73340
ThalesGMO <thalesoliveiramello@hotmail.com>
2025-10-15 17:15:43 -0300

    adicionando repositorio de reserva

--- Repository/FuncionarioRepository.cs
+++ Repository/FuncionarioRepository.cs
@@ -1,5 +1,3 @@
-using Microsoft.Data.SqlClient;
-using SistemaHospedagem.Extensions;
 using SistemaHospedagem.Models;
 
 namespace SistemaHospedagem.Repository;
@@ -10,75 +8,6 @@ public class FuncionarioRepository
     {
         var dbConnection = DbConnection.GetConnection();
         var comando = dbConnection.CreateCommand();
-        comando.CommandText = @"INSERT INTO Funcionarios (Nome, Cpf, Telefone, DataAdmissao, Cargo, Status)
-                                    VALUES  (@Nome, @Cpf, @Telefone, @DataAdmissao, @Cargo, @Status)";
-        comando.ExecuteNonQuery();
-        dbConnection.Close();
-    }
-
-    public void Delete(Funcionario funcionario)
-    {
-        var dbConnection = DbConnection.GetConnection();
-        var comando = dbConnection.CreateCommand();
-        comando.CommandText = @"DELETE FROM Funcionarios
-                                WHERE Id = @Id";
-        comando.ExecuteNonQuery();
-        dbConnection.Close();
-    }
-
-    public void Update(Funcionario funcionario)
-    {
-        var dbConnection = DbConnection.GetConnection();
-        var comando = dbConnection.CreateCommand();
-        comando.CommandText = @"UPDATE Funcionarios
-                                    SET Nome = @Nome,
-                                        Cpf = @Cpf,
-                                        Telefone = @Telefone,
-                                        DataAdmissao = @DataAdmissao,
-                                        IdStatus = @Status 
-                                        IdCargo = @Cargo
-                                    WHERE Id = @Id";
-        comando.Parameters.AddWithValue("@Id", funcionario.Id);
-        comando.Parameters.AddWithValue("@Nome", funcionario.Nome);
-        comando.Parameters.AddWithValue("@Cpf", funcionario.Cpf);
-        comando.Parameters.AddWithValue("@Telefone", funcionario.Telefone);
-        comando.Parameters.AddWithValue("@DataAdmissao", funcionario.DataAdmissao);
-        comando.Parameters.AddWithValue("@Status", funcionario.IdStatus);
-        comando.Parameters.AddWithValue("@Cargo", funcionario.IdCargo);
-        comando.ExecuteNonQuery();
-        dbConnection.Close();
-    }
-
-    public IEnumerable<Funcionario> Search()
-    {
-        var funcionarios = new List<Funcionario>();
-        var dbConnection = DbConnection.GetConnection();
-        var comando = dbConnection.CreateCommand();
-        comando.CommandText = @"SELECT  Id,
-                                        Nome,
-                                        Cpf,
-                                        Telefone,
-                                        DataAdmissao,
-                                        IdStatus,
-                                        IdCargo,
-                                    FROM Funcionarios";
-        using (SqlDataReader reader = comando.ExecuteReader())
-        {
-            while (reader.Read())
-            {
-                var funcionario = new Funcionario
-                {
-                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                    Nome = reader.GetString(reader.GetOrdinal("Nome")),
-                    Telefone = reader.GetString(reader.GetOrdinal("Telefone")),
-                    DataAdmissao = reader.GetDateOnly(reader.GetOrdinal("DataAdmissao")),
-                    IdStatus = reader.GetInt32(reader.GetOrdinal("IdStatus")),
-                    IdCargo = reader.GetInt32(reader.GetOrdinal("IdCargo")),
-                };
-                funcionarios.Add(funcionario);
-            }
-        }
-        dbConnection.Close();
-        return funcionarios;
+        comando.CommandText = @"INSERT ";
     }
 }
--- Repository/ReservaRepository.cs
+++ Repository/ReservaRepository.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using SistemaHospedagem.Extensions;
 using SistemaHospedagem.Models;
 
 namespace SistemaHospedagem.Repository
@@ -15,11 +17,11 @@ namespace SistemaHospedagem.Repository
             comando.CommandText = @"INSERT INTO Reservas (IdStatus, IdAcomodacao, IdConta, IdFuncionario, DataCheckin, DataCheckout)
                                             VALUES (@IdStatus, @IdAcomodacao, @IdConta, @IdFuncionario, @DataCheckin, @DataCheckout)";
             comando.Parameters.AddWithValue("@IdStatus", reserva.IdStatus);
-            comando.Parameters.AddWithValue("@IdStatus", reserva.IdAcomodacao);
-            comando.Parameters.AddWithValue("@IdStatus", reserva.IdConta);
-            comando.Parameters.AddWithValue("@IdStatus", reserva.IdFuncionario);
-            comando.Parameters.AddWithValue("@IdStatus", reserva.DataCheckin);
-            comando.Parameters.AddWithValue("@IdStatus", reserva.DataCheckout);
+            comando.Parameters.AddWithValue("@IdAcomodacao", reserva.IdAcomodacao);
+            comando.Parameters.AddWithValue("@IdConta", reserva.IdConta);
+            comando.Parameters.AddWithValue("@IdFuncionario", reserva.IdFuncionario);
+            comando.Parameters.AddWithValue("@DataCheckin", reserva.DataCheckin);
+            comando.Parameters.AddWithValue("@DataCheckout", reserva.DataCheckout);
             comando.ExecuteNonQuery();
             dbConnection.Close();
         }
@@ -33,6 +35,63 @@ namespace SistemaHospedagem.Repository
             comando.Parameters.AddWithValue("@Id", reserva.Id);
             comando.ExecuteNonQuery();
             dbConnection.Close();
+        }
+
+        public void Update(Reserva reserva)
+        {
+            var dbConnection = DbConnection.GetConnection();
+            var comando = dbConnection.CreateCommand();
+            comando.CommandText = @"UPDATE Reservas
+                                        SET IdStatus = @IdStatus,
+                                            IdAcomodacao = @IdAcomodacao,
+                                            IdConta = @IdConta,
+                                            IdFuncionario = @IdFuncionario,
+                                            DataCheckin = @DataCheckin,
+                                            DataCheckout = @DataCheckout
+                                        WHERE Id = @Id";
+            comando.Parameters.AddWithValue("@IdStatus", reserva.IdStatus);
+            comando.Parameters.AddWithValue("@IdAcomodacao", reserva.IdAcomodacao);
+            comando.Parameters.AddWithValue("@IdConta", reserva.IdConta);
+            comando.Parameters.AddWithValue("@IdFuncionario", reserva.IdFuncionario);
+            comando.Parameters.AddWithValue("@DataCheckin", reserva.DataCheckin);
+            comando.Parameters.AddWithValue("@DataCheckout", reserva.DataCheckout);
+            comando.ExecuteNonQuery();
+            dbConnection.Close();
+        }
+
+        public IEnumerable<Reserva> Search()
+        {
+            var reservas = new List<Reserva>();
+            var dbConnection = DbConnection.GetConnection();
+            var comando = dbConnection.CreateCommand();
+            comando.CommandText = @"SELECT 
+                                        Id,
+                                        IdStatus,
+                                        IdAcomodacao,
+                                        IdConta,
+                                        IdFuncionario,
+                                        DataCheckin,
+                                        DataCheckout
+                                    FROM Reservas";
+            using (SqlDataReader reader = comando.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var reserva = new Reserva()
+                    {
+                        Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                        IdStatus = reader.GetInt32(reader.GetOrdinal("IdStatus")),
+                        IdAcomodacao = reader.GetInt32(reader.GetOrdinal("IdAcomodacao")),
+                        IdConta = reader.GetInt32(reader.GetOrdinal("IdConta")),
+                        IdFuncionario = reader.GetInt32(reader.GetOrdinal("IdFuncionario")),
+                        DataCheckin = reader.GetDateOnly(reader.GetOrdinal("DataCheckin")),
+                        DataCheckout = reader.GetDateOnly(reader.GetOrdinal("DataCheckout")),
+                    };
+                    reservas.Add(reserva);
+                }
+            }
+            dbConnection.Close();
+            return reservas; 
         }
     }
 }
